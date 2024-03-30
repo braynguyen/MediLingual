f5e737f528cc3974da74ea65a663e75f64bf308a
braynguyen <btn16@pitt.edu>
2024-03-30 19:02:06 -0400

    cleans up text real niceeee

--- audio/fixInput.py
+++ audio/fixInput.py
@@ -10,25 +10,28 @@ def load_env(filename=".env"):
             if line and not line.startswith("#"):
                 key, value = line.split("=", 1)
                 env[key.strip()] = value.strip()
-#     return env
+    return env
 
 def fix_input(text):
     env_vars = load_env()
-    open_ai_key = env_vars.get("QUENTINS_OPEN_AI_KEY")
-    client = OpenAI()
+    # open_ai_key = env_vars.get("QUENTINS_OPEN_AI_KEY")
+    client = OpenAI(
+    # This is the default and can be omitted
+        api_key=env_vars.get("QUENTINS_OPEN_AI_KEY"),
+    )
 
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
             {"role": "system", "content": "You are a text cleaner for a bedside translator."},
-            {"role": "user", "content": "Clean up this text to make it suitable for a doctor or a nurse to read: " + text}
+            {"role": "user", "content": "The following text contains the output from a audio to text from a conversation between a doctor and a patient. Due to the limitations of the audio to text model it may contain sentences that do not make sense, or a grammatically incorrect. Your job is to be a professional transcriptionist. Output a logical and grammatically correct edited version of the text." + text}
         ]
     )
 
-    print(completion.choices[0].message)
+    print(completion.choices[0].message.content.strip('"'))
 
 
 if __name__ == "__main__":
     text = input("Give an example prompt to clean up:\n")
-    
-    # fix_input(text)
+
+    fix_input(text)
