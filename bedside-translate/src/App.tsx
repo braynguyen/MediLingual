bd9f6f163469117ecedd23133563521aefc2abf4
Quentin Romero Lauro <quentinromero@outlook.com>
2024-03-30 19:22:51 -0400

    complete front-end overall

--- bedside-translate/src/App.tsx
+++ bedside-translate/src/App.tsx
@@ -4,14 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="app-container">
-      <div className="row">
-        <div className="panel">Panel 1</div>
-        <div className="panel">Panel 2</div>
-      </div>
-      <div className="row">
-        <div className="panel">Panel 3</div>
-        <div className="panel">Panel 4</div>
-      </div>
+      <div className="panel">Panel 1</div>
+      <div className="panel">Panel 2</div>
+      <div className="panel">Panel 3</div>
+      <div className="panel">Panel 4</div>
     </div>
   );
 }
