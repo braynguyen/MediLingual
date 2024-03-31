ecaffb5f5e3aad5becfc06512fbfd4a1a58894a1
braynguyen <btn16@pitt.edu>
2024-03-30 21:16:17 -0400

    comments

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,6 @@
 from flask import app, Flask
 from speechToText import transcribe_long_running_audio
+from testOpenAISTT import speech_to_text
 from recordAudio import create_audio
 from fixInput import fix_input
 
@@ -10,9 +11,13 @@ app = Flask(__name__)
 def main():
     # create_audio()
 
-    text = transcribe_long_running_audio()
+    # original speech to text
+    text = speech_to_text()
 
+    # cleans up speech to text
     fixed_input = fix_input(text)
+    
+    print(fixed_input)
 
 if __name__ == '__main__':
     app.run()
