0a8ca13ac4ea7d86b49b4d90f58e68f0b0d1a8d3
braynguyen <btn16@pitt.edu>
2024-04-01 12:07:58 -0400

    changed name

--- front-end/src/components/sidebar/index.jsx
+++ front-end/src/components/sidebar/index.jsx
@@ -145,7 +145,7 @@ const Sidebar = ({ open,
 
       <div className="mx-[56px] mt-[50px] flex items-center">
         <div className="mt-1 ml-1 h-2.5 font-poppins text-[26px] font-bold uppercase text-navy-700 dark:text-white">
-          Parthiv <span className="font-medium">Patel</span>
+          Medi <span className="font-medium">Lingual</span>
         </div>
       </div>
       <div className="mt-[58px] mb-7 h-px bg-gray-300 dark:bg-white/30" />
